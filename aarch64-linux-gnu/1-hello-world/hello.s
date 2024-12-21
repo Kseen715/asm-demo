44809e029f681d263626d4ffca3f95ef034fcba9
Kseen715 <korenevdn@gmail.com>
2024-12-22 01:07:36 +0300

    Code formatting

--- aarch64-linux-gnu/1-hello-world/hello.s
+++ aarch64-linux-gnu/1-hello-world/hello.s
@@ -3,18 +3,17 @@
 .global _start
 
 .section .data
-msg:	.ascii	"Hello World!\n"
-len = . - msg
+msg:    .ascii "Hello World!\n"
+    len = .- msg
 
 .section .text
 _start:
-	mov x0, #1		// 1 - stdout
-	ldr x1, =msg	// Adress of the string
-	mov x2, len 	// Length of the string
-	mov x8, #64		// syscall for write
-	svc 0			// Call the kernel
-	
-	mov x0, #0		// 0 - exit code
-	mov x8, #93		// syscall for exit
-	svc 0
-    
+    mov x0, #1                  // 1 - stdout
+    ldr x1, =msg                // Adress of the string
+    mov x2, len                 // Length of the string
+    mov x8, #64                 // syscall for write
+    svc 0                       // Call the kernel
+
+    mov x0, #0                  // 0 - exit code
+    mov x8, #93                 // syscall for exit
+    svc 0
